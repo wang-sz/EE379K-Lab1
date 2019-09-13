cc2dfbaf16534eec57df86a505e5ecadafc1e5f9
sean <sean.wang@utexas.edu>
2019-09-13 15:58:23 -0500

    Add script part4

--- part4/split_get.py
+++ part4/split_get.py
@@ -10,5 +10,21 @@ def main():
   
   client.connect((dst_host,dst_port))
 
-  
-  
+  # GET request of the search, split into 2 packets (1 character per packet)
+  pkt_1 = "GET /service/site/search.aspx?query=%E6%B3%95"
+  pkt_2 = "%E8%BD%AE HTTP/1.1\r\n"
+
+  # send packets
+  client.send(pkt_1.encode())
+  client.send(pkt_2.encode())
+
+  # recv data
+  resp = client.recv(4096)
+  http_resp = repr(resp)
+  http_resp_len = len(http_resp)
+
+  print("[RECV] length: "+str(http_resp_len))
+  print(http_resp)
+
+if __name__ == "__main__":
+  main()
